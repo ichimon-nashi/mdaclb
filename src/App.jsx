a7cc5909f3fba068c67063e9e71094c9bd893b21
Eric Han <ichimon-nashi@gmail.com>
2025-04-26 16:33:23 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { clbData } from "./clbData.js";
+import noImage from "./assets/noimage.jpg";
+
+function App() {
+  const [query, setQuery] = useState("");
+
+  // --------SEARCH INPUT FILTER--------
+  const handleInputChange = (e) => {
+    setQuery(e.target.value).toLowerCase();
+    console.log(e.target.value)
+  }
+
+  const handleReset = () => {
+    setQuery("");
+  }
+
+  const filteredCLBItems = clbData.map((item, index) => {
+    if ((item.chineseText.toLowerCase().includes(query)) || (item.englishText.toLowerCase().includes(query))) {
+      return (
+        <div key={index} className="carousel-item">
+          <div className="card bg-base-100 shadow-sm">
+            {item.img !== "" ? <img className="cardImg" src={item.img} /> : <img className="cardImg" src={noImage} />}
+            <div className="card-body">
+              <h2 className="card-title">{item.englishText}</h2>
+              <p>{item.chineseText}</p>
+            </div>
+          </div>
+        </div>
+      )
+    } 
+  })
+
+  return (
+    <>
+      <div className="heading">
+        <h1>
+          <span className="neon">CLB</span> Reference Card
+        </h1>
+        <small>&lt;&gt;部分請自行改成適當位置/時機</small>
+      </div>
+
+      <div className="searchbox">
+        <label className="searchInput input w-96">
+          <svg
+            className="magnifyingIcon h-[1.2rem] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"><g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g>
+          </svg>
+          <input type="search" placeholder="搜尋" value={query} onChange={handleInputChange} />
+        </label>
+        <button className="resetBtn" onClick={handleReset}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
+          </svg>
+        </button>
+      </div>
+
+      <div className="carousel carousel-center rounded-box">
+        {filteredCLBItems}
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/clbData.js
@@ -0,0 +1,48 @@
+import clb1 from "./assets/clb1.png";
+import clb2 from "./assets/clb2.png"
+
+export const clbData = [
+    {
+        "img": clb1,
+        "chineseText" :"<F1>話筒裂掉",
+        "englishText" : "<F1> HANDSET CRACKED",
+    },
+    {
+        
+        "img": clb2,
+        "chineseText":"<3C>扶手傾斜按鈕卡住",
+        "englishText": "<3C>RECLINE BUTTON STUCK",
+    },
+    {
+        "img": "",
+        "chineseText" :"<6C>椅背傾斜功能故障",
+        "englishText" : "<6C>RECLINE MECHANISM INOP",
+    },
+    {
+        
+        "img": "",
+        "chineseText":"後貨艙防煙隔簾魔鬼氈不粘",
+        "englishText": "AFT CARGO ANTI-SMOKE DIVIDER VELCRO NON ADHESIVE",
+    },
+    {
+        "img": "",
+        "chineseText" :"<8A>餐桌裂掉",
+        "englishText" : "<8A>TRAY TABLE CRACKED",
+    },
+    {
+        
+        "img": "",
+        "chineseText":"<8A>餐桌不平",
+        "englishText": "<8A>TRAY TABLE UNLEVELED",
+    },
+    {
+        "img": "",
+        "chineseText" :"<9A>扶手橡皮蓋鬆脫",
+        "englishText" : "<9A>ARMREST RUBBER COVER LOOSE",
+    },
+    {
+        "img": "",
+        "chineseText" :"地毯髒",
+        "englishText" : "CARPET DIRTY",
+    },
+]
